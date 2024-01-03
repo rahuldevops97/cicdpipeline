49fcceb030868c57f80ef79da66ef69014759526
rahuldevops97 <123821605+rahuldevops97@users.noreply.github.com>
2024-01-03 15:43:57 +0530

    Create book.py

---
+++ book.py
@@ -0,0 +1,9 @@
+books = [
+   {'title': 'The Catcher in the Rye', 'author': 'J.D. Salinger', 'year': 1951, 'genre': 'Fiction'},
+   {'title': 'To Kill a Mockingbird', 'author': 'Harper Lee', 'year': 1960, 'genre': 'Fiction'},
+   {'title': '1984', 'author': 'George Orwell', 'year': 1949, 'genre': 'Dystopian Fiction'},
+]
+
+
+for book in books:
+   print(book['title'])
